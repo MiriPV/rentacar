e481e58f6ba1a3e1a717065899275c9f592404de
Miranda <mirandaverheij@gmail.com>
2022-01-16 15:41:49 +0100

    working view

--- app/src/main/java/nl/avans/rent_my_car/ui/cars/CarListFragment.kt
+++ app/src/main/java/nl/avans/rent_my_car/ui/cars/CarListFragment.kt
@@ -1,16 +1,16 @@
 package nl.avans.rent_my_car.ui.cars
 
+import CustomAdapter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import nl.avans.rent_my_car.databinding.FragmentCarListBinding
 import androidx.recyclerview.widget.LinearLayoutManager
-import nl.avans.rent_my_car.CustomAdapter
+import nl.avans.rent_my_car.R
 
 
 class CarListFragment : Fragment() {
@@ -25,20 +25,25 @@ class CarListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        carViewModel =
-            ViewModelProvider(this)[CarViewModel::class.java]
+       // carViewModel =
+        //    ViewModelProvider(this)[carViewModel]
 
         _binding = FragmentCarListBinding.inflate(inflater, container, false)
         val root: View = binding.root
         val recyclerView: RecyclerView = binding.carList
         recyclerView.layoutManager = LinearLayoutManager(this.context)
-        val adapter = CustomAdapter(arrayOf("mercedes", "BMW", "audi"))
+        //val adapter = CustomAdapter(arrayOf("mercedes", "BMW", "audi"))
+        // ArrayList of class ItemsViewModel
+        val data = ArrayList<CarViewModel>()
+
+        for (i in 1..20) {
+            data.add(CarViewModel(R.drawable.ic_baseline_view_list_24, "Item " + i))
+        }
+
+        val adapter = CustomAdapter(data)
 
         recyclerView.adapter = adapter
 
-        //carViewModel.text.observe(viewLifecycleOwner, Observer {
-            //textView.text = it
-        //})
         return root
     }
 
